071479ec3feaaf099ae3e20df20b87927eb87365
Natan Lao <natanlao@users.noreply.github.com>
2018-05-09 19:15:43 +0000

    ssh-agent functionality fixes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,7 @@
 alias g git
 setenv EDITOR vim
 
-# Start ssh-agent
-# Adapted from https://superuser.com/a/996128
-if test -z "(command pgrep ssh-agent)" -a -S !\($SSH_AUTH_SOCK\)
+# Start ssh-agent (from https://superuser.com/a/996128)
+if begin; test -z (command pgrep ssh-agent); and not test -S $SSH_AUTH_SOCK; end
     eval (command ssh-agent -c | sed -E 's/^setenv (.+);$/set \1; set -Ux \1;/')
 end
