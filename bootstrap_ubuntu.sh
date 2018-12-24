fea0f5723e088e7704734817cb734c49b17c8c17
Natan Lao <natanlao@users.noreply.github.com>
2018-12-24 13:58:13 -0800

    Fix Sublime Text install bug
    sublime-text depends on libgtk2.0-0, which is not automatically installed

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -21,7 +21,7 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 sudo apt install -y apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
-sudo apt install -y sublime-text
+sudo apt install -y sublime-text libgtk2.0-0
 
 # Install dotfiles
 stow -t ~ fish
