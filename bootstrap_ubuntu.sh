7760d97c80a5bd46649175628e5771119d6e4b6c
Natan Lao <natanlao@users.noreply.github.com>
2018-12-24 03:16:58 -0800

    Update bootstrap_ubuntu.sh
    - Improve Sublime Text install process
    - Pare installed packages
    - Install `ack` with apt

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -7,7 +7,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Install software
 sudo apt update
-sudo apt install -y git fish vim git wget tmux python3 mosh stow python-dev build-essential python-pip xautolock i3 xclip firefox xinit network-manager bluez
+sudo apt install -y git fish vim git wget tmux python3 mosh stow python-dev build-essential python3-pip xautolock i3 xclip firefox xinit ack
 
 # Set fish as the default shell
 sudo sh -c 'echo `which fish` >> /etc/shells'
@@ -18,10 +18,10 @@ pip install virtualenv || sudo pip install virtualenv
 
 # Install sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install -y apt-transport-https
+sudo apt install -y apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install -y sublime-text
+sudo apt update
+sudo apt install -y sublime-text
 
 # Install dotfiles
 stow -t ~ fish
@@ -30,8 +30,3 @@ stow -t ~ vim
 stow -t ~ i3
 mkdir -p ~/.config/sublime-text-3/
 stow -t ~/.config/sublime-text-3/ sublime
-
-echo
-echo
-echo "Manually install: playerctl, ack-grep, i3lock-color, light"
-
