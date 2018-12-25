09c57e45bc5a8c906e5452ad89c90267d101a3f1
Natan Lao <natanlao@users.noreply.github.com>
2018-12-24 18:36:20 -0800

    Add virtualenv helpers

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,18 @@
 alias g git
 setenv EDITOR vim
+
+function venv
+    if test -n "$argv"
+        python3 -m venv $argv
+    else
+        python3 -m venv venv
+    end
+end
+
+function vact
+    source $argv/bin/activate.fish
+end
+
 # These aliases should only be activate if the current platform
 # is Ubuntu so we don't clobber macOS's builtin pbcopy/pbaste
 if python -m platform | grep -qi Ubuntu
