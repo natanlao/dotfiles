9c8f39ca196131c04f620be3851b99cf59cbe838
Natan Lao <natanlao@users.noreply.github.com>
2021-05-03 20:26:31 -0700

    Work around dhcpcd delay during boot
    A more robust solution might be to configure a static IP instead, but
    I'm too lazy.

--- hosts/kyoshi/configuration.nix
+++ hosts/kyoshi/configuration.nix
@@ -49,12 +49,23 @@ in {
     interval = "weekly";
   };
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
+
   networking.useDHCP = false;
   networking.interfaces.enp4s0.useDHCP = true;
-  networking.interfaces.wlp5s0.useDHCP = true;
+  networking.interfaces.wlp5s0.useDHCP = false;
+  # https://github.com/NixOS/nixpkgs/issues/60900
+  networking.dhcpcd.enable = false;
+  systemd.network.enable = true;
+  systemd.network.networks = {
+    internet0 = {
+      matchConfig = {
+        Name = "enp4s0";
+      };
+      networkConfig = {
+        DHCP = "ipv4";
+      };
+    };
+  };
 
   # Enable pulseaudio
   sound.enable = true;
