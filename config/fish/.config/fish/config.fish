f7194cfe2b7543784776964f98849043b96d82ac
Natan Lao <natanlao@users.noreply.github.com>
2021-05-07 20:53:37 -0700

    Refactor fish aliases/functions

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -1,5 +1,17 @@
-alias g git
 setenv EDITOR vim
+
+alias f "feh --auto-zoom --scale-down --draw-filename --draw-tinted"
+
+alias g "git"
+
+if ! type -q pbcopy and ! type -q pbpaste
+    alias pbcopy "xclip -selection clipboard"
+    alias pbpaste "xclip -selection clipboard -o"
+end
+
+function vact
+    source $argv/bin/activate.fish
+end
 
 function venv
     if test -n "$argv"
@@ -8,20 +20,3 @@ function venv
         python3 -m venv venv
     end
 end
-
-function vact
-    source $argv/bin/activate.fish
-end
-
-# These aliases should only be activate if the current platform
-# is Ubuntu so we don't clobber macOS's builtin pbcopy/pbaste
-if python -m platform | grep -qi Ubuntu
-    alias pbcopy "xclip -selection clipboard"
-    alias pbpaste "xclip -selection clipboard -o"
-end
-
-function assume
-    eval (assume-role $argv)
-end
-
-pyenv init - | source
