f9929bef0ca3aa42cabaf0ce8292c3878658c1f8
Natan Lao <natanlao@users.noreply.github.com>
2022-01-05 00:54:31 -0800

    Only define fish aliases if executable exists

--- cfg/.config/fish/config.fish
+++ cfg/.config/fish/config.fish
@@ -1,23 +1,36 @@
 setenv EDITOR vim
 
-alias f "feh --auto-zoom --scale-down --draw-filename --draw-tinted"
+# This config is used across many different machines, so it's a good idea to
+# be careful not to clobber anything by accident.
 
-alias g "git"
+if command -sq feh
+    alias f "feh --auto-zoom --scale-down --draw-filename --draw-tinted"
+end
 
-if ! which pbcopy pbpaste > /dev/null 2>&1
+if command -sq git
+    alias g git
+end
+
+if command -sq terraform
+    alias tf terraform
+end
+
+if ! command -sq pbcopy; and ! command -sq pbpaste; and command -sq xclip
     alias pbcopy "xclip -selection clipboard"
     alias pbpaste "xclip -selection clipboard -o"
 end
 
-function vact
-    source $argv/bin/activate.fish
-end
+if command -sq python3
+    function vact
+        source $argv/bin/activate.fish
+    end
 
-function venv
-    if test -n "$argv"
-        python3 -m venv $argv
-    else
-        python3 -m venv venv
+    function venv
+        if test -n "$argv"
+            python3 -m venv $argv
+        else
+            python3 -m venv venv
+        end
     end
 end
 
