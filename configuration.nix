6e84074993077b777a0d673b583cabb4fd233021
Natan Lao <natanlao@users.noreply.github.com>
2024-08-27 22:05:53 -0700

    Add dnscrypt-proxy2

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,51 @@
   networking.firewall.enable = true;
   networking.wireless.enable = false;
   networking.wireless.userControlled.enable = false;
-  networking.nameservers = lib.mkDefault ["9.9.9.9" "149.112.112.112"];
+
+  ### DNS
+
+  networking.nameservers = ["127.0.0.1" "::1"];
+
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    upstreamDefaults = false;
+
+    settings = {
+      block_unqualified = true;
+      cache = true;
+      dnscrypt_servers = true;
+      doh_servers = true;
+      ignore_system_dns = true;
+      ipv4_servers = true;
+      ipv6_servers = false;
+      lb_strategy = "p2";
+      listen_addresses = [ "127.0.0.1:53" ];
+      max_clients = 50;
+      require_dnssec = true;
+      require_nofilter = false;
+      require_nolog = true;
+
+      server_names = [
+        "cloudflare-security"
+        "mullbad-base-doh"
+        "quad9-dnscrypt-ip4-filter-ecs-pri"
+        "wikimedia"
+      ];
+
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+    };
+  };
+
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy";
+  };
 
   ## Banish caps lock everywhere
 
